b4d3c4732c825d8c5c53069fe470d5e0bd67f219
Rae Knowler <rae.knowler@liip.ch>
2022-07-20 17:19:13 +0200

    Use jQuery for less flashing

--- javascript/background.js
+++ javascript/background.js
@@ -1,5 +1,10 @@
 const changeBackground = setInterval(function () {
     const imageId = Math.floor(Math.random() * 14);
 
-    document.body.style.backgroundImage = "url(../images/image_" + imageId.toString() + ".jpg)";
+    var image = new Image();
+    image.onload = function () {
+        $("body").css("background-image", "url('" + image.src + "')");
+    }
+
+    image.src = "images/image_" + imageId.toString() + ".jpg";
 }, 10 * 1000);
