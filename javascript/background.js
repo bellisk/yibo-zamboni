7bfa4b33e89e0aefd7edc4b1fb132635b6377a44
Rae Knowler <rae.knowler@liip.ch>
2022-07-20 18:28:53 +0200

    Split wide photo into two

--- javascript/background.js
+++ javascript/background.js
@@ -1,10 +1,9 @@
 const changeBackground = setInterval(function () {
-    const imageId = Math.floor(Math.random() * 14);
+    const imageId = Math.floor(Math.random() * 15);
 
     var image = new Image();
     image.onload = function () {
         $("body").css("background-image", "url('" + image.src + "')");
     }
-
     image.src = "images/image_" + imageId.toString() + ".jpg";
 }, 10 * 1000);
